43d8aa65d89641b6c5b51c44d82550e9d4529f49
motopikl <clara.lille57@gmail.com>
2021-10-28 17:00:17 +0900

    1
    1

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ var data = {
     image_width: "300px",
     // Image source: you can use local files, or a link
     // Ex.: image_source: "/img/kanji.jpg"
-    image_source: "https://i.imgur.com/C7yCYUq.jpg",
+    image_source: "https://i.imgur.com/zPmHW4F.jpeg",
 
     // -------------------------------------------------
     // Enable/disable site icons
